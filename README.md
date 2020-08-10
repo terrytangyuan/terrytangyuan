83ac8b4013e6069f98860c6cf0e89a7968ea85b1
terrytangyuan <terrytangyuan@gmail.com>
2020-08-10 08:53:04 -0400

    Add script to update citation
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 	<a href="https://www.zhihu.com/people/terrytangyuan"><img src="https://img.shields.io/badge/知乎--_.svg?style=social&logo=zhihu" alt="知乎"></a>
 	<a href="https://weibo.com/5681818134"><img src="https://img.shields.io/badge/微博--_.svg?style=social&logo=sina-weibo" alt="微博"></a>
 </p>
+
---
+++ scrape-profile-stats.R
@@ -0,0 +1,14 @@
+library("rvest")
+library('xml2')
+
+gscholar_link <- "https://scholar.google.com/citations?user=2GYttqUAAAAJ&hl=en"
+citations <- read_html(gscholar_link) %>%
+  html_nodes("#gsc_rsb_st") %>%
+  .[[1]] %>%
+  html_table() %>%
+  .[1, "All"]
+
+citations_txt <- sprintf("%sk", floor(citations / 1000))
+readme_txt <- readLines("README.md")
+readme_txt <- gsub("Scholar-\\d?k?-_.svg", sprintf("Scholar-%s-_.svg", citations_txt), readme_txt)
+writeLines(readme_txt, con = "README.md")
