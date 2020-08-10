8e39a53b5083356ed64e80a40464b36fc7f5447e
terrytangyuan <terrytangyuan@gmail.com>
2020-08-10 09:48:32 -0400

    More concise code
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -1,13 +1,17 @@
 library(rvest)
 
 gscholar_link <- "https://scholar.google.com/citations?user=2GYttqUAAAAJ&hl=en"
+readme_loc <- "README.md"
+
 citations <- read_html(gscholar_link) %>%
   html_nodes("#gsc_rsb_st") %>%
   .[[1]] %>%
   html_table() %>%
   .[1, "All"]
 
-citations_txt <- sprintf("%sk", floor(citations / 1000))
-readme_txt <- readLines("README.md")
-readme_txt <- gsub("Scholar-\\d?k?-_.svg", sprintf("Scholar-%s-_.svg", citations_txt), readme_txt)
-writeLines(readme_txt, con = "README.md")
+readme_txt <- readLines(readme_loc)
+readme_txt <- gsub(
+  "Scholar-\\d?k?-_.svg",
+  sprintf("Scholar-%sk-_.svg", floor(citations / 1000)),
+  readme_txt)
+writeLines(readme_txt, con = readme_loc)
