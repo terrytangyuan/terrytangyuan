87e000aeed7921efbe8e152b34eb28e4c1093d53
terrytangyuan <terrytangyuan@gmail.com>
2021-03-13 23:55:55 -0500

    Add sponsors count
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -9,12 +9,14 @@ citations <- read_html(gscholar_link) %>%
   html_table() %>%
   .[1, "All"]
 
+sponsors_num <- jsonlite::fromJSON(rawToChar(curl::curl_fetch_memory("https://sponsors.trnck.dev/terrytangyuan/count")$content))$sponsors$count
+
 # Download images in advance so we don't rely on img.shields.io at rendering time.
 imgs <- list(
   github = "https://img.shields.io/github/followers/terrytangyuan.svg?label=GitHub&style=social",
   twitter = "https://img.shields.io/twitter/follow/TerryTangYuan?label=Twitter&style=social",
   linkedin = "https://img.shields.io/badge/LinkedIn--_.svg?style=social&logo=linkedin",
-  sponsors = "https://img.shields.io/badge/Sponsors--_.svg?style=social&logo=github&logoColor=EA4AAA",
+  sponsors = sprintf("https://img.shields.io/badge/Sponsors-%s-_.svg?style=social&logo=github&logoColor=EA4AAA", sponsors_num),
   citations = sprintf("https://img.shields.io/badge/Citations-%sk-_.svg?style=social&logo=google-scholar", round(citations / 1000, digits = 1)),
   zhihu = sprintf("https://img.shields.io/badge/%s--_.svg?style=social&logo=zhihu", URLencode("知乎", reserved = TRUE)),
   weibo = sprintf("https://img.shields.io/badge/%s--_.svg?style=social&logo=sina-weibo", URLencode("微博", reserved = TRUE))
