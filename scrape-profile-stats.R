a695a51f2bd38dd766d80f420cf36c4e7e39e9cd
Yuan Tang <terrytangyuan@gmail.com>
2023-01-21 17:29:39 -0500

    Temporarily disable twitter log
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -13,7 +13,7 @@ citations <- read_html(gscholar_link) %>%
 imgs <- list(
   cv = "https://img.shields.io/badge/CV--_.svg?style=social&logo=giphy",
   github = "https://img.shields.io/github/followers/terrytangyuan.svg?label=GitHub&style=social",
-  twitter = "https://img.shields.io/twitter/follow/TerryTangYuan?label=Twitter&style=social",
+  # twitter = "https://img.shields.io/twitter/follow/TerryTangYuan?label=Twitter&style=social",
   linkedin = "https://img.shields.io/badge/LinkedIn-4k-_.svg?style=social&logo=linkedin",
   mastodon = "https://img.shields.io/mastodon/follow/109697385486067962?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social",
   sponsors = sprintf("https://img.shields.io/github/sponsors/terrytangyuan?label=Sponsors&style=social&logoColor=EA4AAA"),
