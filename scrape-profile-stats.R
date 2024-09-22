34fd52474b9ab6dd5b66d9ba3bc796622e25fc82
Yuan Tang <terrytangyuan@gmail.com>
2024-09-21 23:38:32 -0400

    Update scrape-profile-stats.R

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -13,7 +13,7 @@ citations <- read_html(gscholar_link) %>%
 imgs <- list(
   cv = "https://img.shields.io/badge/Curriculum%20Vitae--_.svg?style=social&logo=giphy",
   github = "https://img.shields.io/github/followers/terrytangyuan.svg?label=GitHub&style=social",
-  twitter = "https://img.shields.io/badge/X-9k-_.svg?style=social&logo=x",
+  twitter = "https://img.shields.io/badge/X-9.1k-_.svg?style=social&logo=x",
   linkedin = "https://img.shields.io/badge/LinkedIn-7.2k-_.svg?style=social&logo=linkedin",
   mastodon = "https://img.shields.io/mastodon/follow/109697385486067962?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social",
   sponsors = sprintf("https://img.shields.io/github/sponsors/terrytangyuan?label=Sponsors&style=social&logoColor=EA4AAA"),
