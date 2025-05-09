4190f67ae1aca824252e5f025daff55b06d4aecc
Yuan Tang <terrytangyuan@gmail.com>
2025-05-08 22:41:40 -0400

    Update scrape-profile-stats.R

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -1,25 +1,25 @@
-library(rvest)
+# TODO: This no longer works as Google Scholar started blocking
+# library(rvest)
+# gscholar_link <- "https://scholar.google.com/citations?user=2GYttqUAAAAJ&hl=en"
+# citations <- gscholar_link %>%
+#   httr::GET(config = httr::config(ssl_verifypeer = FALSE)) %>%
+#   read_html() %>%
+#   html_nodes("#gsc_rsb_st") %>%
+#   .[[1]] %>%
+#   html_table() %>%
+#   .[1, "All"]
 
-gscholar_link <- "https://scholar.google.com/citations?user=2GYttqUAAAAJ&hl=en"
 readme_loc <- "README.md"
-
-citations <- gscholar_link %>%
-  httr::GET(config = httr::config(ssl_verifypeer = FALSE)) %>%
-  read_html() %>%
-  html_nodes("#gsc_rsb_st") %>%
-  .[[1]] %>%
-  html_table() %>%
-  .[1, "All"]
 
 # Download images in advance so we don't rely on img.shields.io at rendering time.
 imgs <- list(
   cv = "https://img.shields.io/badge/Curriculum%20Vitae--_.svg?style=social&logo=giphy",
   github = "https://img.shields.io/github/followers/terrytangyuan.svg?label=GitHub&style=social",
-  # Numbers for Twitter/X and LinkedIn need to be updated manually
+  # Numbers for X, LinkedIn, citations need to be updated manually
   twitter = "https://img.shields.io/badge/X-9k-_.svg?style=social&logo=x",
   linkedin = "https://img.shields.io/badge/LinkedIn-11k-_.svg?style=social&logo=linkedin",
   sponsors = sprintf("https://img.shields.io/github/sponsors/terrytangyuan?label=Sponsors&style=social&logoColor=EA4AAA"),
-  citations = sprintf("https://img.shields.io/badge/Citations-%sk-_.svg?style=social&logo=google-scholar", round(citations / 1000, digits = 1))
+  citations = sprintf("https://img.shields.io/badge/Citations-8.3k-_.svg?style=social&logo=google-scholar")
 )
 
 for (i in 1:length(imgs)) {
