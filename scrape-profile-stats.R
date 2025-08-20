608d6a20f018bc445c7709733539f95ee6f53c96
Yuan Tang <terrytangyuan@gmail.com>
2025-08-20 09:17:19 -0400

    Update scrape-profile-stats.R

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -19,7 +19,7 @@ imgs <- list(
   mastodon = "https://img.shields.io/mastodon/follow/109697385486067962?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social",
   # Numbers for X, LinkedIn, citations need to be updated manually
   twitter = "https://img.shields.io/badge/X-9k-_.svg?style=social&logo=x",
-  linkedin = "https://img.shields.io/badge/LinkedIn-13k-_.svg?style=social&logo=linkedin",
+  linkedin = "https://img.shields.io/badge/LinkedIn-14k-_.svg?style=social&logo=linkedin",
   citations = sprintf("https://img.shields.io/badge/Citations-8.8k-_.svg?style=social&logo=google-scholar")
 )
 
