d01a1895f97c07a18e9363b6d51e587274b1ceb6
Yuan Tang <terrytangyuan@gmail.com>
2025-09-15 22:33:42 -0400

    Update Twitter follower count in profile stats
    Updated Twitter follower count from 9.1k to 9.2k.

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -18,7 +18,7 @@ imgs <- list(
   sponsors = sprintf("https://img.shields.io/github/sponsors/terrytangyuan?label=Sponsors&style=social&logoColor=EA4AAA"),
   mastodon = "https://img.shields.io/mastodon/follow/109697385486067962?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social",
   # Numbers for X, LinkedIn, citations need to be updated manually
-  twitter = "https://img.shields.io/badge/X-9.1k-_.svg?style=social&logo=x",
+  twitter = "https://img.shields.io/badge/X-9.2k-_.svg?style=social&logo=x",
   linkedin = "https://img.shields.io/badge/LinkedIn-15k-_.svg?style=social&logo=linkedin",
   citations = sprintf("https://img.shields.io/badge/Citations-8.8k-_.svg?style=social&logo=google-scholar")
 )
