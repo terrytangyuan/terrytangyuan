bc6cabca68e3f2e0fb0aa013a8341b34c2210949
Yuan Tang <terrytangyuan@gmail.com>
2025-10-14 06:44:00 -0400

    Update scrape-profile-stats.R

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -21,7 +21,7 @@ imgs <- list(
   twitter = "https://img.shields.io/badge/X-9.2k-_.svg?style=social&logo=x",
   linkedin = "https://img.shields.io/badge/LinkedIn-16k-_.svg?style=social&logo=linkedin",
   citations = "https://img.shields.io/badge/Citations-8.8k-_.svg?style=social&logo=google-scholar",
-  substack = "https://img.shields.io/badge/Substack-623-_.svg?style=social&logo=substack"
+  substack = "https://img.shields.io/badge/Substack-702-_.svg?style=social&logo=substack"
 )
 
 for (i in 1:length(imgs)) {
