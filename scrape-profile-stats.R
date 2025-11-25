8c71abfa102d7d1b7ebbf23dde613532e9b264bb
Yuan Tang <terrytangyuan@gmail.com>
2025-11-25 16:54:40 -0500

    Update LinkedIn and Substack badge counts

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -19,9 +19,9 @@ imgs <- list(
   mastodon = "https://img.shields.io/mastodon/follow/109697385486067962?domain=https%3A%2F%2Ffosstodon.org&label=Mastodon&style=social",
   # Numbers for X, LinkedIn, citations, and Substack need to be updated manually
   twitter = "https://img.shields.io/badge/X-9.5k-_.svg?style=social&logo=x",
-  linkedin = "https://img.shields.io/badge/LinkedIn-18k-_.svg?style=social&logo=linkedin",
+  linkedin = "https://img.shields.io/badge/LinkedIn-19k-_.svg?style=social&logo=linkedin",
   citations = "https://img.shields.io/badge/Citations-9.4k-_.svg?style=social&logo=google-scholar",
-  substack = "https://img.shields.io/badge/Substack-950-_.svg?style=social&logo=substack"
+  substack = "https://img.shields.io/badge/Substack-975-_.svg?style=social&logo=substack"
 )
 
 for (i in 1:length(imgs)) {
