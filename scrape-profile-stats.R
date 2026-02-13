e84b5aeabcca43082e9374c8568bc0b55aa5c23c
Yuan Tang <terrytangyuan@gmail.com>
2026-02-13 16:57:32 -0500

    Update fallback value for substack formatted followers

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -6,7 +6,7 @@ gscholar_link <- "https://scholar.google.com/citations?user=2GYttqUAAAAJ&hl=en"
 
 # These fallback values will be used when they cannot be extracted from existing SVG files or scraped from websites
 total_followers_fallback <- "53.4k"
-substack_formatted_fallback <- "1.2k"
+substack_formatted_fallback <- "1.3k"
 citations_formatted_fallback <- "9.4k"
 
 # Numbers for X and LinkedIn need to be updated manually
