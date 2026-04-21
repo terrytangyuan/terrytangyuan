3ebe50572c8ef9b543da567ba9702e2bed1e7dd6
Yuan Tang <terrytangyuan@gmail.com>
2026-04-20 22:48:07 -0400

    Update scrape-profile-stats.R

--- scrape-profile-stats.R
+++ scrape-profile-stats.R
@@ -11,7 +11,7 @@ citations_formatted_fallback <- "12.3k"
 
 # Numbers for X and LinkedIn need to be updated manually
 twitter_followers <- "9.9k"
-linkedin_followers <- "22.5k"
+linkedin_followers <- "23k"
 
 # Helper function to create HTTP GET request with browser headers
 make_browser_request <- function(url) {
